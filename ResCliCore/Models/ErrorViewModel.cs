8669bab0f2bf45d5b849f6cc23799baace6cce38
jac2206 <arango773@gmail.com>
2022-06-13 20:09:24 -0500

    Cliente residuos core

---
+++ ResCliCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResCliCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
